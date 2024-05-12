298149df39ed25aa9cb34efdf14dc3416c3600da
Felix Vasquez <felixevr.dev@gmail.com>
2024-05-11 21:56:05 -0500

    ConCategory Dtos, BaseResponse, Validator and ReplayMessage classes

---
+++ POS.Application/Dtos/Response/CategoryResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using POS.Domain.Entities;
+
+namespace POS.Application.Dtos.Response
+{
+    public class CategoryResponseDto
+    {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime AuditCreateDate { get; set; }
+        public int State { get; set; }
+        public string? StateCategory { get; set; } 
+    }
+}
