d990bf11723fb811286bba4845434c73d88491ff
Felix Vasquez <felixevr.dev@gmail.com>
2024-05-17 23:26:38 -0500

    Adding Category Mapper and several DI services to InjectionExtensions

---
+++ POS.Application/Mappers/CategoryMapingsProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using POS.Application.Dtos.Response;
+using POS.Domain.Entities;
+using POS.Infrastructure.Commons.Bases.Response;
+using POS.Utilities.Static;
+
+namespace POS.Application.Mappers
+{
+    public class CategoryMapingsProfile : Profile
+    {
+        public CategoryMapingsProfile()
+        {
+            CreateMap<Category, CategoryResponseDto>()
+                .ForMember(x => x.StateCategory, x => x.MapFrom(y => y.State.Equals((int)StateTypes.Active) ? "Activo" : "Inactivo"))
+                .ReverseMap();
+
+            CreateMap<BaseEntityResponse<Category>, BaseEntityResponse<CategoryResponseDto>>()
+                .ReverseMap();
+
+            CreateMap<CategoryResponseDto, Category>();
+
+            CreateMap<CategorySelectResponseDto, Category>()
+                .ReverseMap();
+        }
+    }
+}
